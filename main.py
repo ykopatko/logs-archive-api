13987d9bab65609c4d1075e4828e5d515ee04e9f
ykopatko <cs.kopatko.evgen@gmail.com>
2023-08-25 16:21:41 +0300

    Edit main.py file to conifgure initial app setup

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 
+from api import api_router
+from core.database import engine, Base
+from fastapi_pagination import add_pagination
+
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(api_router)
+add_pagination(app)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.on_event("startup")
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
