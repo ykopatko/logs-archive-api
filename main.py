6241e2d857d011eb1e171dd1e1c1c90c609bb9c4
ykopatko <cs.kopatko.evgen@gmail.com>
2023-08-26 12:35:58 +0300

    Fix main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 
 from api import api_router
 from core.database import engine, Base
-from fastapi_pagination import add_pagination
 
 app = FastAPI()
 
 app.include_router(api_router)
-add_pagination(app)
 
 
 @app.on_event("startup")
